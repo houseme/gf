be2a807f3b519d33901449a1cc2c242eb9576b46
houseme <qzg40737@163.com>
2022-02-10 20:44:59 +0800

    feat: create registry center zookeeper

---
+++ contrib/registry/zookeeper/go.mod
@@ -0,0 +1,10 @@
+module github.com/gogf/gf/contrib/registry/zookeeper/v2
+
+go 1.16
+
+require (
+	github.com/go-zookeeper/zk v1.0.2
+	github.com/gogf/gf/v2 v2.0.0-rc2.0.20220129134248-c3161d58fdad
+)
+
+replace github.com/gogf/gf/v2 => ../../../
---
+++ contrib/registry/zookeeper/watcher.go
@@ -0,0 +1,37 @@
+package zookeeper
+
+import (
+	"context"
+
+	"github.com/gogf/gf/v2/net/gsvc"
+)
+
+var _ gsvc.Watcher = &watcher{}
+
+type watcher struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+	event  chan struct{}
+	set    *serviceSet
+}
+
+func (w *watcher) Proceed() (services []*gsvc.Service, err error) {
+	select {
+	case <-w.ctx.Done():
+		err = w.ctx.Err()
+	case <-w.event:
+	}
+	ss, ok := w.set.services.Load().([]*gsvc.Service)
+	if ok {
+		services = append(services, ss...)
+	}
+	return
+}
+
+func (w *watcher) Close() error {
+	w.cancel()
+	w.set.lock.Lock()
+	defer w.set.lock.Unlock()
+	delete(w.set.watcher, w)
+	return nil
+}
